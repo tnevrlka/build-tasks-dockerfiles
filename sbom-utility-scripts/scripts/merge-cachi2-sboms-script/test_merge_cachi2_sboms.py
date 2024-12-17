16a429deb353d9771cf2b95db177f61806ac8a13
Adam Cmiel <acmiel@redhat.com>
2024-12-17 10:02:51 +0100

    merge_cachi2_sboms: make test more human-friendly
    The SBOM files are too large to inspect by hand. Check the property that
    we care about most - the components present in the merged SBOM that were
    not present in the cachi2 SBOM.
    
    Note: this uncovers some bugs (the gomod and npm packages shouldn't be
    there, they're duplicates). To be fixed later.
    
    Also disable the flake8 line-length check; it is useless since we
    already use 'black' for formatting.
    
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- sbom-utility-scripts/scripts/merge-cachi2-sboms-script/test_merge_cachi2_sboms.py
+++ sbom-utility-scripts/scripts/merge-cachi2-sboms-script/test_merge_cachi2_sboms.py
@@ -36,6 +36,10 @@ def data_dir() -> Path:
     return Path(__file__).parent / "test_data"
 
 
+def get_purls(sbom: dict[str, Any]) -> set[str]:
+    return {component["purl"] for component in sbom["components"]}
+
+
 def test_merge_sboms(data_dir: Path) -> None:
     result = merge_sboms(f"{data_dir}/cachi2.bom.json", f"{data_dir}/syft.bom.json")
 
@@ -43,6 +47,42 @@ def test_merge_sboms(data_dir: Path) -> None:
         expected_sbom = json.load(file)
 
     assert json.loads(result) == expected_sbom
+
+    with open(f"{data_dir}/cachi2.bom.json") as f:
+        cachi2_sbom = json.load(f)
+
+    purls_taken_from_syft_sbom = get_purls(expected_sbom) - get_purls(cachi2_sbom)
+    assert purls_taken_from_syft_sbom == {
+        "pkg:golang/github.com/cachito-testing/gomod-pandemonium@v0.0.0#terminaltor",
+        "pkg:golang/github.com/cachito-testing/gomod-pandemonium@v0.0.0#weird",
+        "pkg:golang/github.com/cachito-testing/retrodep@v2.1.1#v2",
+        "pkg:golang/github.com/onsi/ginkgo@v2.9.2#v2",
+        "pkg:golang/github.com/release-engineering/retrodep@v2.1.0#v2",
+        "pkg:npm/eggs-packages/eggs@1.0.0",
+        "pkg:npm/spam-packages/spam@1.0.0",
+        "pkg:rpm/rhel/basesystem@11-13.el9?arch=noarch&upstream=basesystem-11-13.el9.src.rpm&distro=rhel-9.5",
+        "pkg:rpm/rhel/bash@5.1.8-9.el9?arch=x86_64&upstream=bash-5.1.8-9.el9.src.rpm&distro=rhel-9.5",
+        "pkg:rpm/rhel/coreutils-single@8.32-36.el9?arch=x86_64&upstream=coreutils-8.32-36.el9.src.rpm&distro=rhel-9.5",
+        "pkg:rpm/rhel/filesystem@3.16-5.el9?arch=x86_64&upstream=filesystem-3.16-5.el9.src.rpm&distro=rhel-9.5",
+        "pkg:rpm/rhel/glibc-common@2.34-125.el9_5.1?arch=x86_64&upstream=glibc-2.34-125.el9_5.1.src.rpm&distro=rhel-9.5",
+        "pkg:rpm/rhel/glibc-minimal-langpack@2.34-125.el9_5.1?arch=x86_64&upstream=glibc-2.34-125.el9_5.1.src.rpm&distro=rhel-9.5",
+        "pkg:rpm/rhel/glibc@2.34-125.el9_5.1?arch=x86_64&upstream=glibc-2.34-125.el9_5.1.src.rpm&distro=rhel-9.5",
+        "pkg:rpm/rhel/gpg-pubkey@5a6340b3-6229229e?distro=rhel-9.5",
+        "pkg:rpm/rhel/gpg-pubkey@fd431d51-4ae0493b?distro=rhel-9.5",
+        "pkg:rpm/rhel/libacl@2.3.1-4.el9?arch=x86_64&upstream=acl-2.3.1-4.el9.src.rpm&distro=rhel-9.5",
+        "pkg:rpm/rhel/libattr@2.5.1-3.el9?arch=x86_64&upstream=attr-2.5.1-3.el9.src.rpm&distro=rhel-9.5",
+        "pkg:rpm/rhel/libcap@2.48-9.el9_2?arch=x86_64&upstream=libcap-2.48-9.el9_2.src.rpm&distro=rhel-9.5",
+        "pkg:rpm/rhel/libgcc@11.5.0-2.el9?arch=x86_64&upstream=gcc-11.5.0-2.el9.src.rpm&distro=rhel-9.5",
+        "pkg:rpm/rhel/libselinux@3.6-1.el9?arch=x86_64&upstream=libselinux-3.6-1.el9.src.rpm&distro=rhel-9.5",
+        "pkg:rpm/rhel/libsepol@3.6-1.el9?arch=x86_64&upstream=libsepol-3.6-1.el9.src.rpm&distro=rhel-9.5",
+        "pkg:rpm/rhel/ncurses-base@6.2-10.20210508.el9?arch=noarch&upstream=ncurses-6.2-10.20210508.el9.src.rpm&distro=rhel-9.5",
+        "pkg:rpm/rhel/ncurses-libs@6.2-10.20210508.el9?arch=x86_64&upstream=ncurses-6.2-10.20210508.el9.src.rpm&distro=rhel-9.5",
+        "pkg:rpm/rhel/pcre2-syntax@10.40-6.el9?arch=noarch&upstream=pcre2-10.40-6.el9.src.rpm&distro=rhel-9.5",
+        "pkg:rpm/rhel/pcre2@10.40-6.el9?arch=x86_64&upstream=pcre2-10.40-6.el9.src.rpm&distro=rhel-9.5",
+        "pkg:rpm/rhel/redhat-release@9.5-0.6.el9?arch=x86_64&upstream=redhat-release-9.5-0.6.el9.src.rpm&distro=rhel-9.5",
+        "pkg:rpm/rhel/setup@2.13.7-10.el9?arch=noarch&upstream=setup-2.13.7-10.el9.src.rpm&distro=rhel-9.5",
+        "pkg:rpm/rhel/tzdata@2024b-2.el9?arch=noarch&upstream=tzdata-2024b-2.el9.src.rpm&distro=rhel-9.5",
+    }
 
 
 @pytest.mark.parametrize(
