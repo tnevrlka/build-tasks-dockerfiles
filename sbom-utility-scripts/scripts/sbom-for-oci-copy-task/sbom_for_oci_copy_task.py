5ff3539070795decc423073516d3456027581359
Adam Cmiel <acmiel@redhat.com>
2025-01-14 12:41:26 +0100

    sbom_for_oci_copy: support SPDX
    Note: the top-level attributes in the SPDX SBOM are mostly based on the
    index_image_sbom_script script
    
    Note 2: the SBOM's top-level name attribute and the fake_root package
    are mostly irrelevant, because this script should be used in tandem with
    the add_image_reference script, which will replace both.
    
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- sbom-utility-scripts/scripts/sbom-for-oci-copy-task/sbom_for_oci_copy_task.py
+++ sbom-utility-scripts/scripts/sbom-for-oci-copy-task/sbom_for_oci_copy_task.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 import argparse
+import datetime
+import hashlib
 import json
+import re
+import uuid
 from typing import IO, Any, TypedDict
 
 import yaml
@@ -47,6 +51,65 @@ def to_cyclonedx_sbom(artifacts: list[Artifact]) -> dict[str, Any]:
     }
 
 
+def to_spdx_package(artifact: Artifact) -> dict[str, Any]:
+    purl = to_purl(artifact)
+    purl_hex_digest = hashlib.sha256(purl.encode()).hexdigest()
+    # based on a validation error from https://github.com/spdx/tools-java
+    #   Invalid SPDX ID: ...  Must match the pattern SPDXRef-([0-9a-zA-Z\.\-\+]+)$
+    sanitized_filename = re.sub(r"[^0-9a-zA-Z\.\-\+]", "-", artifact["filename"])
+    return {
+        "SPDXID": f"SPDXRef-Package-{sanitized_filename}-{purl_hex_digest}",
+        "name": artifact["filename"],
+        "externalRefs": [
+            {
+                "referenceType": "purl",
+                "referenceLocator": purl,
+                "referenceCategory": "PACKAGE-MANAGER",
+            },
+        ],
+        "checksums": [{"algorithm": "SHA256", "checksumValue": artifact["sha256sum"]}],
+        "downloadLocation": artifact["source"],
+    }
+
+
+def to_spdx_sbom(artifacts: list[Artifact]) -> dict[str, Any]:
+    real_packages = list(map(to_spdx_package, artifacts))
+
+    def relationship(a: str, relationship_type: str, b: str) -> dict[str, Any]:
+        return {"spdxElementId": a, "relationshipType": relationship_type, "relatedSpdxElement": b}
+
+    # The only purpose of this package is to be the "root" of the relationships graph
+    fake_root = {
+        "SPDXID": "SPDXRef-DocumentRoot-Unknown",
+        "downloadLocation": "NOASSERTION",
+        "name": "",
+    }
+
+    relationships = [relationship("SPDXRef-DOCUMENT", "DESCRIBES", fake_root["SPDXID"])]
+    relationships.extend(relationship(fake_root["SPDXID"], "CONTAINS", package["SPDXID"]) for package in real_packages)
+
+    packages = [fake_root] + real_packages
+
+    return {
+        "spdxVersion": "SPDX-2.3",
+        "dataLicense": "CC0-1.0",
+        "documentNamespace": f"https://konflux-ci.dev/spdxdocs/sbom-for-oci-copy-task/{uuid.uuid4()}",
+        "SPDXID": "SPDXRef-DOCUMENT",
+        "creationInfo": {
+            "created": _datetime_utc_now().strftime("%Y-%m-%dT%H:%M:%SZ"),
+            "creators": ["Tool: Konflux"],
+        },
+        "name": "sbom-for-oci-copy-task",
+        "packages": packages,
+        "relationships": relationships,
+    }
+
+
+def _datetime_utc_now() -> datetime.datetime:
+    # a mockable datetime.datetime.now
+    return datetime.datetime.now(datetime.UTC)
+
+
 def main() -> None:
     ap = argparse.ArgumentParser()
     ap.add_argument("oci_copy_yaml", type=argparse.FileType(), default="-")
@@ -64,7 +127,7 @@ def main() -> None:
     if sbom_type == "cyclonedx":
         sbom = to_cyclonedx_sbom(artifacts)
     else:
-        raise NotImplementedError("SPDX not implemented")
+        sbom = to_spdx_sbom(artifacts)
 
     json.dump(sbom, output_file, indent=2)
     output_file.write("\n")
